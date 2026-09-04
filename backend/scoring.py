a7da007fd06155ee9e11ac0108d70da4c72d4339
alex <achin5804@gmail.com>
2026-09-03 22:39:10 -0700

    added todos for scoring module

---
+++ backend/scoring.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pathlib
+
+# @ Nghi
+
+def load_centroids(category):
+    # TODO: 
+    # Navigate and load correct centroid files
+    # Return correct centroids as np array
+
+    return np.array([])
+
+def calculate_score(embedding, category):
+    # TODO
+    # Take cosine similarity between embedding and centroids
+    # Return the avg cosine similarity as the score
+
+    return 0
